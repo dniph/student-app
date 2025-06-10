ed8fb93a444037cab930e0cf82768ca16a025378
Dennif <dennif.pa@gmail.com>
2025-06-10 14:01:36 -0700

    studenComponents data stored in array

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -3,13 +3,17 @@ import './StudentList.css';
 import Student from './Student';
 
 const StudentList = () => {
+const studentComponents = [
+    <li><Student name="Ada" email="ada@dev.org"></Student></li>,
+    <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>,
+    <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
+];
+
     return (
         <section>
             <h2>Student List</h2>
             <ul>
-                <li><Student name="Ada" email="ada@dev.org"></Student></li>
-                <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>
-                <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
+                {studentComponents}
             </ul>
         </section>
     )
