6d6afcfefb9672076f836185ca193b4512d094a7
Dennif <dennif.pa@gmail.com>
2025-06-10 18:04:19 -0700

    Iterating Over studenData objects array

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -3,20 +3,35 @@ import './StudentList.css';
 import Student from './Student';
 
 const StudentList = () => {
-const studentComponents = [
-    <li><Student name="Ada" email="ada@dev.org"></Student></li>,
-    <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>,
-    <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
-];
+    const studentData = [
+        {
+            nameData: 'Ada',
+            emailData: 'ada@dev.org'
+        },
+        {
+            nameData: 'Soo-ah',
+            emailData: 'sooah@dev.org'
+        },
+        {
+            nameData: 'Chrissy',
+            emailData: 'chrissy@dev.org'
+        }
+    ];
 
-    return (
-        <section>
-            <h2>Student List</h2>
-            <ul>
-                {studentComponents}
-            </ul>
-        </section>
-    )
-}
+    const studentComponents = studentData.map(student => {
+        return (
+            <li><Student name={student.nameData} email={student.emailData}></Student></li>
+        );
+    });
+
+        return (
+            <section>
+                <h2>Student List</h2>
+                <ul>
+                    {studentComponents}
+                </ul>
+            </section>
+        )
+    }
 
 export default StudentList;
