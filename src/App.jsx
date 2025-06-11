42651d5922e4f6e415d2f81869e26da4fe3d8e21
Dennif <dennif.pa@gmail.com>
2025-06-10 18:53:17 -0700

    number of members prop added in Class info

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <main>
       <h1>Attendance</h1>
-      <ClassInfo></ClassInfo>
+      <ClassInfo memberCount={studentData.length}></ClassInfo>
       <StudentList students={studentData}></StudentList>
     </main>
   );
