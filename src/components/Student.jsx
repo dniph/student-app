1d794831eec9bb298e1161cad41390f06ade4521
Dennif <dennif.pa@gmail.com>
2025-06-16 21:06:01 -0700

    Single Source of Truth implemented

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+
 import PropTypes from 'prop-types';
 import './Student.css';
 
 const Student = (props) => {
-    const [isPresent, setIsPresent] = useState(false);
 
-    const togglePresence = () => {
-        setIsPresent(!isPresent);
-    };
-
-    const nameColor = isPresent ? 'green' : 'red';
+    const attendanceButtonClicked = () => {
+        // Invoke the function passed in through the prop named "onPresenceToggle"
+        // This function refers to the toggleStudentPresence function in App
+        props.onPresenceToggle(props.id);
+};
+    const nameColor = props.isPresent ? 'green' : 'red';
 
     return (
         <div>
@@ -17,14 +17,17 @@ const Student = (props) => {
                 <li className={nameColor}>Nickname: {props.name}</li>
                 <li>Email: {props.email}</li>
             </ul>
-            <button onClick={togglePresence}>Toggle if {props.name} is present</button>
+            <button onClick={attendanceButtonClicked}>Toggle if {props.name} is present</button>
         </div>
     );
 };
 
 Student.propTypes = {
+    id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired
+    email: PropTypes.string.isRequired,
+    isPresent: PropTypes.bool.isRequired,
+    onPresenceToggle: PropTypes.func.isRequired,
 };
 
 export default Student;
